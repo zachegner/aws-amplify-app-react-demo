6106eebea852e275b98161ec42bd16e5c64ec379
kongman551 <eggy551@gmail.com>
2022-05-16 13:38:52 -0400

    added auth

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import { Authenticator } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
 
 function App() {
   return (
+    <Authenticator>
+      {({ signOut, user }) => (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello from V2
+          We now have auth
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
+      )}
+      </Authenticator>
   );
 }
 
